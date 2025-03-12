596001bb95cb544d6cd5dfdeeb81ee250b75be54
cresas <137892152+cresas@users.noreply.github.com>
2025-03-12 08:27:11 +0300

    Add files via upload

---
+++ panel/upload/temp/8047.js
@@ -0,0 +1,135 @@
+{
+  "scale": "human",
+  "events": [
+    {
+      "start_date": {
+        "year": "1914",
+        "month": "6",
+        "day": "28"
+      },
+      "text": {
+        "headline": "Saraybosna Suikasti",
+        "text": "<p>28 Haziran 1914 sabahı Avusturya arşidükünün alayı Saraybosna'daki belediye binasına doğru ilerlerken üzerilerine bir bomba atıldı. Arşidük, ikinci arabada oturuyordu ve bu patlamadan yara almadan kurtuldu. Alay, geri dönerken genç bir Bosnalı Sırp olan Gavrilo Princip'in beklediği Schiller's Store'un önünden geçti. Princip, iki el ateş ederek Avusturya tahtının varisiyle onun eşini ölümcül şekilde yaraladı.</p><span class=\"tl-note\">Gerçekleştirilen saldırı, Avusturya varlığına karşı düşmanlık besleyen bir grup genç Boşnak milliyetçisi tarafından düzenlendi. Sonraki haftalarda Viyana hükûmetinin bu etkinliği, tüm güney Slav halklarını daha büyük bir Yugoslavya'da birleştirmeyi planlayan ve böylece Avusturya-Macaristan İmparatorluğu'nun birliğini tehdit eden Sırbistan'a meydan okumak için kullanıldı.</span> "
+      },
+      "media": {
+       
+      
+        "url": "https://i.hizliresim.com/ddo79wc.png"
+
+        
+      },
+      "overlay": {
+        "url": "https://i.hizliresim.com/l1rqaed.jpg",
+        "position": "top-right",
+        "opacity": 0.6
+      }
+    },
+    {
+      "start_date": {"year": "1914", "month": "7", "day": "28"},
+      "text": {
+        "headline": "Avusturya'nın Sırbistan'a Savaş İlanı",
+        "text": "28 Temmuz'da Viyana, Sırbistan'a savaş ilan etti ve Belgrad'ı bombaladı."
+      },
+      "media": {
+        "url": "<iframe src=\"https://intmaps1.vercel.app/\" width=\"100%\" height=\"100%\" frameborder=\"0\"></iframe>"
+      }
+    },
+    {
+      "start_date": {"year": "1914", "month": "7", "day": "30"},
+      "text": {
+        "headline": "Rusya Genel Seferberlik İlan Etti",
+        "text": "30 Temmuz'da Sırbistan'ın koruyucusu Rusya genel seferberlik ilan etti."
+      },
+      "media": {
+        "url": "<iframe src=\"https://intmaps2.vercel.app/\" width=\"100%\" height=\"600px\" frameborder=\"0\"></iframe>"
+      }
+    },
+    {
+      "start_date": {"year": "1914", "month": "8", "day": "1"},
+      "text": {
+        "headline": "Almanya Rusya'ya Savaş İlan Etti",
+        "text": "1 Ağustos'ta Avusturya'nın müttefiki Almanya, Rusya'ya savaş ilan etti, Fransa da askerlerini harekete geçirdi."
+      },
+      "media": {
+        "url": "<iframe src=\"https://intmaps3.vercel.app/\" width=\"100%\" height=\"600px\" frameborder=\"0\"></iframe>"
+      }
+    },
+    {
+      "start_date": {"year": "1914", "month": "8", "day": "3"},
+      "text": {
+        "headline": "Almanya Fransa'ya Savaş İlan Etti",
+        "text": "3 Ağustos'ta Almanya, Fransa'ya savaş ilan etti."
+      },
+      "media": {
+        "url": "<iframe src=\"https://intmaps4.vercel.app/\" width=\"100%\" height=\"600px\" frameborder=\"0\"></iframe>"
+      }
+    },
+    {
+      "start_date": {"year": "1914", "month": "8", "day": "4"},
+      "text": {
+        "headline": "İngiltere Savaşa Katıldı",
+        "text": "4 Ağustos'ta Almanya'nın Belçika'nın tarafsızlığını ihlal etmesi, İngiltere'yi Fransa ve Rusya'nın yanında savaşa girmeye zorladı."
+      },
+      "media": {
+        "url": "<iframe src=\"https://intmaps5.vercel.app/\" width=\"100%\" height=\"600px\" frameborder=\"0\"></iframe>"
+      }
+    },
+    {
+      "start_date": {"year": "1914", "month": "11"},
+      "text": {
+        "headline": "Osmanlı İmparatorluğu Savaşa Girdi",
+        "text": "Kısa sürmesi gereken savaş, dört yıldan fazla sürdü ve giderek diğer savaşan taraflara yayıldı. Osmanlı İmparatorluğu Kasım 1914’te savaşa girdi,"
+      },
+      "media": {
+        "url": "<iframe src=\"https://intmaps6.vercel.app/\" width=\"100%\" height=\"600px\" frameborder=\"0\"></iframe>"
+      }
+    },
+    
+    {
+      "start_date": {"year": "1915", "month": "5"},
+      "text": {
+        "headline": "İtalya ve Bulgaristan Savaşa Girdi ",
+        "text": "Önce İtalya ardından Bulgaristan Mayıs-Ekim 1915’te savaşa girdi."
+      },
+      "media": {
+        "url": "<iframe src=\"https://intmaps7.vercel.app/\" width=\"100%\" height=\"600px\" frameborder=\"0\"></iframe>"
+      }
+    },
+    
+        {
+      "start_date": {"year": "1916", "month": "3"},
+      "text": {
+        "headline": "Portekiz ve Romanya Savaşa Girdi ",
+        "text": "Portekiz ve Romanya Mart-Eylül 1916’da savaşa girdi."
+      },
+      "media": {
+        "url": "<iframe src=\"https://intmaps8.vercel.app/\" width=\"100%\" height=\"600px\" frameborder=\"0\"></iframe>"
+      }
+    },
+    
+        {
+      "start_date": {"year": "1917", "month": "4", "day": "6"},
+      "text": {
+        "headline": "ABD Almanya'ya Savaş İlan Etti",
+        "text": "ABD'nin Nisan 1917'de Almanya'ya savaş ilan etmesi, savaşa Avrupa'daki sömürge imparatorluklarını da kapsayan küresel bir statü kazandırdı."
+      },
+      "media": {
+        "url": "<iframe src=\"https://intmaps9.vercel.app/\" width=\"100%\" height=\"600px\" frameborder=\"0\"></iframe>"
+      }
+    },
+    
+    
+    
+      
+      {
+      "start_date": {"year": "1917", "month": "6"},
+      "text": {
+        "headline": "Yunanistan Savaşa Girdi ",
+        "text": "Yunanistan ise Haziran 1917'de savaşa girdi"
+      },
+      "media": {
+        "url": "<iframe src=\"https://intmaps10.vercel.app/\" width=\"100%\" height=\"600px\" frameborder=\"0\"></iframe>"
+      }
+    }
+  ]
+}
